69aeb0cdf325613ae15374b5d97b8af6a61d9628
Irwin Gonzales <irwingonzales@ymail.com>
2017-09-26 11:14:37 -0700

    Guest List Work

--- HangSwift/HomeVC.swift
+++ HangSwift/HomeVC.swift
@@ -882,6 +882,11 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "locationCell", for: indexPath) as! FriendSearchCell
         
+//        for var user in guestArray
+//        {
+//            
+//        }
+        
         cell.usernameLabel.text = guestArray[indexPath.row]
         
         
@@ -918,7 +923,7 @@ extension HomeVC: UITableViewDelegate, UITableViewDataSource
         }))
         
         alertVC.addAction(PMAlertAction(title: "OK", style: .default, action: { () in
-            UpdateService.instance.addUsersIntoGuestList(users: self.guestArray)
+//            UpdateService.instance.addUsersIntoGuestList(users: self.guestArray)
 
             print("Capture action OK")
             print("Friend is added!")
